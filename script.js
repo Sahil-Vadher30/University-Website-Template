7bfea9b8153d0da29c3659598d4ede5d9a8b174a
Sahil-Vadher30 <122278312+Sahil-Vadher30@users.noreply.github.com>
2023-02-23 21:18:15 +0530

    updated

---
+++ script.js
@@ -0,0 +1,13 @@
+// function login(){
+//     var i=0;
+//     if(i%2==0){
+//         // document.getElementById('login').innerHTML='hello';
+//         document.getElementById('login').style.visibility='visible';
+//         i++;
+//     }
+//     else{
+//         // document.getElementById('login').innerHTML='';
+//         document.getElementById('login').style.visibility='hidden';
+//         i++;
+//     }
+// }
